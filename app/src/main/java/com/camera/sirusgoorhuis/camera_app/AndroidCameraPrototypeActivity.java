205111d3956becf2c09303e74df76b36598f9af3
Marie-Rose Heijkoop <shadowphoenix@SilverPhoenix.local>
2018-05-25 09:02:44 +0200

    Added button to main activity for android camera prototype

---
+++ app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidCameraPrototypeActivity.java
@@ -0,0 +1,13 @@
+package com.camera.sirusgoorhuis.camera_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidCameraPrototypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_camera_prototype);
+    }
+}
