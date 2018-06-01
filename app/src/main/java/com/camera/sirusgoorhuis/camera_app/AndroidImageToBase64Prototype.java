fd16a7882c63044c6cbc223e049e7bba0036695c
Unknown <s.goorhuis@student.fontys.nl>
2018-06-01 11:28:46 +0200

    Added view for ImageToBase64 Prototype

---
+++ app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidImageToBase64Prototype.java
@@ -0,0 +1,13 @@
+package com.camera.sirusgoorhuis.camera_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidImageToBase64Prototype extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_image_to_base64_prototype);
+    }
+}
