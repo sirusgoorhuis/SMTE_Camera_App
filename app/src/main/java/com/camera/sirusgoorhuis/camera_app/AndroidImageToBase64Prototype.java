ced7b0285ed6d1ce014da41961c588d77ebd3107
Unknown <s.goorhuis@student.fontys.nl>
2018-06-01 15:04:49 +0200

    Tried to make Image to Base64 work
    Still not working... pushing to be able to work in another branch

--- app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidImageToBase64Prototype.java
+++ app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidImageToBase64Prototype.java
@@ -5,15 +5,37 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 
 public class AndroidImageToBase64Prototype extends AppCompatActivity {
 
+    Button toBase64, toImage;
+    TextView outputText;
+    String outputString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_image_to_base64_prototype);
+
+        outputText = (TextView) findViewById(R.id.outputBase64);
+
+        toBase64 = (Button) findViewById(R.id.toBase64);
+        toImage = (Button) findViewById(R.id.toImage);
+
+        toBase64.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              todo: can't find the image path, gets a nullpointer exception when the button "IMAGE TO BASE64" is pressed
+                outputString = imageToBase64("drawable://" + R.drawable.harold);
+                outputText.setText(outputString);
+            }
+        });
+
     }
 
     // IMAGE TO BASE64
