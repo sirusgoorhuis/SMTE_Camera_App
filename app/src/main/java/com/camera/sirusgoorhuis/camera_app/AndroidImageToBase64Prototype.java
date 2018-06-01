1f77f6ac76c6dc91607a43a2cf98075d66bd4306
Unknown <s.goorhuis@student.fontys.nl>
2018-06-01 13:09:38 +0200

    Added Layout For Base64 Prototype

--- app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidImageToBase64Prototype.java
+++ app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidImageToBase64Prototype.java
@@ -1,7 +1,12 @@
 package com.camera.sirusgoorhuis.camera_app;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 public class AndroidImageToBase64Prototype extends AppCompatActivity {
 
@@ -10,4 +15,22 @@ public class AndroidImageToBase64Prototype extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_image_to_base64_prototype);
     }
+
+    // IMAGE TO BASE64
+    private String imageToBase64(String imagePath) {
+        Bitmap bm = BitmapFactory.decodeFile(imagePath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] byteArrayImage = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    // BASE64 TO IMAGE
+    private Bitmap Base64ToImage(String encodedImage) {
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        return decodedByte;
+    }
+
 }
