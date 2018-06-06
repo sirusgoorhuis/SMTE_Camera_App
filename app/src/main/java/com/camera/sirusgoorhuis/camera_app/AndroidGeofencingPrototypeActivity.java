c27fe9ad9cb370d6f6f7b0e62ed9aab06e5e5443
Unknown <s.goorhuis@student.fontys.nl>
2018-06-06 22:27:03 +0200

    Added views for the geofencing

---
+++ app/src/main/java/com/camera/sirusgoorhuis/camera_app/AndroidGeofencingPrototypeActivity.java
@@ -0,0 +1,13 @@
+package com.camera.sirusgoorhuis.camera_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidGeofencingPrototypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_geofencing_prototype);
+    }
+}
